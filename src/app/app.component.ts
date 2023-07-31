009a8bce634966a1312f07efbf5c2536d25e307c
Felipe T. Iwamoto <felipe.t.iwamoto@gmail.com>
2023-07-31 16:51:34 -0300

    routing working on github pages even with refresh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,6 @@ import {Component} from '@angular/core'
 
 @Component({
 	selector: 'app-root',
-	template: `
-		<a routerLink="/landing-pages/01">landing-pages01</a><br />
-		<a routerLink="/projects/01">projects01</a><br />
-		<a routerLink="/resumes/01">resume01</a><br />
-		<br />
-
-		<router-outlet></router-outlet>
-	`,
+	template: `<router-outlet></router-outlet>`,
 })
 export class AppComponent {}
